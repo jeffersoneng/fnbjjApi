c9a76e29c4bf61415b542f334a89911e3d7b3710
jeffersoneng <jeffersoneng2005@gmail.com>
2021-12-13 11:08:33 -0300

    [FEAT] Primeiro Commit

---
+++ fnbjjapi/src/main/java/br/com/jcoder/fnbjjapi/FnbjjapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.jcoder.fnbjjapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FnbjjapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FnbjjapiApplication.class, args);
+    }
+
+}
